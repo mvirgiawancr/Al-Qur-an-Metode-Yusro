7764ece4b2888b18d94ae9d884ff52016c3bc6bf
Moch Virgiawan Caesar Ridollohi <virgiawan.muhamad@gmail.com>
2025-08-21 09:18:49 +0700

    finish halaman login

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Button from "../components/Button";
+import Link from "next/link";
 
 const LoginPage = () => {
   return (
@@ -35,7 +37,7 @@ const LoginPage = () => {
                 name="username"
                 type="text"
                 placeholder="Masukkan username Anda"
-                className="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-lg shadow-sm placeholder-gray-400 focus:outline-none focus:ring-emerald-500 focus:border-emerald-500"
+                className="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-lg shadow-sm placeholder-gray-400 focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 text-black"
               />
             </div>
 
@@ -60,27 +62,25 @@ const LoginPage = () => {
                 name="password"
                 type="password"
                 placeholder="Masukkan kata sandi Anda"
-                className="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-lg shadow-sm placeholder-gray-400 focus:outline-none focus:ring-emerald-500 focus:border-emerald-500"
+                className="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-lg shadow-sm placeholder-gray-400 focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 text-black"
               />
             </div>
 
             {/* Tombol Submit */}
             <div>
-              <button
-                type="submit"
-                className="cursor-pointer w-full flex justify-center py-3 px-4 border border-transparent rounded-lg shadow-sm text-lg font-bold text-white bg-[#059669] hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 transition duration-150 ease-in-out"
-              >
-                Masuk
-              </button>
+              <Button label="Masuk" />
             </div>
           </form>
 
           {/* Link Daftar */}
           <p className="text-center text-sm text-gray-600">
             Belum punya akun?{" "}
-            <a href="#" className="font-medium text-[#059669] hover:underline">
+            <Link
+              href="/register"
+              className="font-medium text-[#059669] hover:underline"
+            >
               Daftar di sini
-            </a>
+            </Link>
           </p>
         </div>
       </main>
