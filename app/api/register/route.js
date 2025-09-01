d27bb10c317b793f38c64cd83cacf71323320d19
Moch Virgiawan Caesar Ridollohi <virgiawan.muhamad@gmail.com>
2025-09-01 11:51:55 +0700

    fix port issues

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -22,6 +22,7 @@ export async function POST(request) {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
+      port: parseInt(process.env.DB_PORT || "3306"),
     });
 
     // Cek apakah email sudah ada
